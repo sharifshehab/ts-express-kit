206e065f56d766829cf946f955bd21d46e406d28
Nasim <mdnasimhosen333@gmail.com>
2025-08-05 09:33:51 +0600

    Implement error handling in getTests method to throw AppError when no tests are found

--- src/app/modules/testModule/test.service.ts
+++ src/app/modules/testModule/test.service.ts
@@ -1,11 +1,17 @@
+import AppError from "../../errors/AppError";
 import Test from "./test.model";
+import httpStatus from "http-status";
 
 
 
 const getTests = async () => {
   //   if you are using a database, you would typically query the database here
   // For example, if using Mongoose: return Test.find();
-  return Test.tests;
+  const tests = await Test.tests;
+  if (!tests) {
+    throw new AppError(httpStatus.NOT_FOUND, "No tests found");
+  }
+  return tests;
 };
 
 const TestService = {
